98b0c16a63b17fbc38abd362dcf3d489004080ca
WillianzD <willianz1999@gmail.com>
2022-08-06 06:57:43 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 const promise = welcome();
 
 console.log(promise);
+
+const question = "Will the weather be nice today?";
+const tellPromise = tell(question);
+
+tellPromise.then(console.log);
